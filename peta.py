ef4337ba3a1cd4be24b6fc6dea6f8add7859de82
girvinjunod <girvinjunod@gmail.com>
2021-04-03 22:28:45 +0700

    fix format file input, ubah euclidean jadi haversine

--- peta.py
+++ peta.py
@@ -1,5 +1,92 @@
 import plotly.graph_objects as go
+#Menggunakan plotly dan mapbox untuk visualisasi map
+
 #px.set_mapbox_access_token('pk.eyJ1IjoiZ2lydmluanVub2QiLCJhIjoiY2tuMDB6ZmFyMGpjOTJubW82ZWJ3em1heCJ9.k5m3PeaIZjpWBM1QUAKeNQ')
+class MapV:
+    def __init__(self, a, b, c): #ctor
+        self.mapbox_access_token = 'pk.eyJ1IjoiZ2lydmluanVub2QiLCJhIjoiY2tuMDB6ZmFyMGpjOTJubW82ZWJ3em1heCJ9.k5m3PeaIZjpWBM1QUAKeNQ'
+        self.map= go.Figure(go.Scattermapbox(
+        lat=a,
+        lon=b,
+        mode='markers',
+        marker= dict(color = 'black', size = 14),
+        text = c,
+        name = "Simpul"
+        ))
+        self.count = 0
+
+    def tambahAwal(self, latawal, lonawal, nawal): #tambah simpul awal
+        self.map.add_trace(go.Scattermapbox(
+            mode = "markers",
+            lat = latawal,
+            lon = lonawal,
+            marker = dict(color = 'LightSkyBlue', size = 16),
+            text = nawal,
+            name = "Awal"
+            ))
+
+    def tambahAkhir(self, latakhir, lonakhir, nakhir): #tambah simpul akhir
+        self.map.add_trace(go.Scattermapbox(
+            mode = "markers",
+            lat = latakhir,
+            lon = lonakhir,
+            marker = dict(color = 'purple', size = 16),
+            text = nakhir,
+            name = "Akhir"
+            ))
+
+    def tambahjalur(self, jalurlat, jalurlon, name): #tambah jalur
+        if (self.count > 0):
+            self.map.add_trace(go.Scattermapbox(
+            mode = "lines",
+            lat = jalurlat,
+            lon = jalurlon,
+            line = dict(color = 'black', width = 2),
+            text = name,
+            legendgroup="a",
+            name = "Jalur",
+            showlegend = False
+            ))
+        else:
+            self.map.add_trace(go.Scattermapbox(
+            mode = "lines",
+            lat = jalurlat,
+            lon = jalurlon,
+            line = dict(color = 'black', width = 2),
+            text = name,
+            legendgroup="a",
+            name = "jalur"
+            ))
+        self.count +=1
+
+    def tambahjalurhasil(self, jalurlat, jalurlon, hasil): #tambah jalur hasil
+        self.map.add_trace(go.Scattermapbox(
+        mode = "lines",
+        lat = jalurlat,
+        lon = jalurlon,
+        line = dict(color = 'red', width = 4),
+        name = "Jalur terdekat",
+        text = hasil
+        ))
+
+    def visualize(self): #gambar map
+        self.map.update_layout(
+        hovermode='closest',
+        title = 'Peta Jalur Terdekat',
+        mapbox=dict(
+            accesstoken=self.mapbox_access_token,
+            bearing=0,
+            center=go.layout.mapbox.Center(
+                lat=44,
+                lon=26
+            ),
+            pitch=0,
+            zoom=5
+            )
+        )
+        self.map.show()
+
+#driver
 if __name__ == '__main__':
     mapbox_access_token = 'pk.eyJ1IjoiZ2lydmluanVub2QiLCJhIjoiY2tuMDB6ZmFyMGpjOTJubW82ZWJ3em1heCJ9.k5m3PeaIZjpWBM1QUAKeNQ'
 
@@ -29,91 +116,4 @@ if __name__ == '__main__':
         lon = ['21.312', '26.093'],
         line = dict(color = 'LightSkyBlue', width = 12)))
 
-
-
-
     fig.show()
-
-class MapV:
-    def __init__(self, a, b, c):
-        self.mapbox_access_token = 'pk.eyJ1IjoiZ2lydmluanVub2QiLCJhIjoiY2tuMDB6ZmFyMGpjOTJubW82ZWJ3em1heCJ9.k5m3PeaIZjpWBM1QUAKeNQ'
-        self.map= go.Figure(go.Scattermapbox(
-        lat=a,
-        lon=b,
-        mode='markers',
-        marker= dict(color = 'black', size = 14),
-        text = c,
-        name = "Simpul"
-        ))
-        self.count = 0
-
-    def tambahAwal(self, latawal, lonawal, nawal):
-        self.map.add_trace(go.Scattermapbox(
-            mode = "markers",
-            lat = latawal,
-            lon = lonawal,
-            marker = dict(color = 'LightSkyBlue', size = 16),
-            text = nawal,
-            name = "Awal"
-            ))
-
-    def tambahAkhir(self, latakhir, lonakhir, nakhir):
-        self.map.add_trace(go.Scattermapbox(
-            mode = "markers",
-            lat = latakhir,
-            lon = lonakhir,
-            marker = dict(color = 'purple', size = 16),
-            text = nakhir,
-            name = "Akhir"
-            ))
-
-    def tambahjalur(self, jalurlat, jalurlon, name):
-        if (self.count > 0):
-            self.map.add_trace(go.Scattermapbox(
-            mode = "lines",
-            lat = jalurlat,
-            lon = jalurlon,
-            line = dict(color = 'black', width = 2),
-            text = name,
-            legendgroup="a",
-            name = "Jalur",
-            showlegend = False
-            ))
-        else:
-            self.map.add_trace(go.Scattermapbox(
-            mode = "lines",
-            lat = jalurlat,
-            lon = jalurlon,
-            line = dict(color = 'black', width = 2),
-            text = name,
-            legendgroup="a",
-            name = "jalur"
-            ))
-        self.count +=1
-
-    def tambahjalurhasil(self, jalurlat, jalurlon, hasil):
-        self.map.add_trace(go.Scattermapbox(
-        mode = "lines",
-        lat = jalurlat,
-        lon = jalurlon,
-        line = dict(color = 'red', width = 4),
-        name = "Jalur terdekat",
-        text = hasil
-        ))
-
-    def visualize(self):
-        self.map.update_layout(
-        hovermode='closest',
-        mapbox=dict(
-            accesstoken=self.mapbox_access_token,
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=44,
-                lon=26
-            ),
-            pitch=0,
-            zoom=5
-            )
-        )
-        self.map.show()
-    
--- tucil3.py
+++ tucil3.py
@@ -4,48 +4,23 @@
 #
 
 from prioqueue import PrioQueue
-#from vgraf import GraphVisualization
 from peta import MapV
+from haversine import haversineDistance
 
 def euclideanDistance(a, b):
     return ((a[0] - b[0])**2 + (a[1] - b[1])**2)**0.5
 
 #input file
-'''
-#nanti diganti jadi parse dari file
-#koordinat tiap simpul
-listkoordinat = [(0,0), (3,4), (3,0), (11,4), (11,-6)] 
-#matrix adj
-matrixjalan = [ #blom ada handle kasus g ada jalan
-[0,1,1,0,0],
-[1,0,1,1,0],
-[1,1,0,0,1],
-[0,1,0,0,1],
-[0,0,1,1,0]
-]
-#nama simpul
-nama = ['A', 'B', 'C', 'D', 'E']
 
-#nama jalan, klo cukup nampilin node aj buang aj
-matrixnama = [
-["-","AB","AC","AD","AE"], #0
-["BA","-","BC","BD","BE"], #1
-["CA","CB","-","CD","CE"], #2
-["DA","DB","DC","-","DE"], #3
-["EA","EB","EC","ED","-"] #4
-]
-'''
 namafile = input("Nama file: ")
 f = open(namafile) #baca soal
 read = f.read().split('\n') #dipisahkan dari newline
 listkoordinat = []
 nama = []
 matrixjalan = []
+n = int(read[0])
 #liat format di txt
-for i in range(len(read)):
-    if ("=" in read[i]):
-        nexti = i+1
-        break
+for i in range(1, n+1):
     idxpostnama = read[i].index("-")
     nama.append(read[i][0:idxpostnama-1])
     read[i] = read[i].replace(" ", "") #hapus spasi
@@ -53,37 +28,29 @@ for i in range(len(read)):
     read[i] = read[i][idxpostnama:]
     templist = read[i].split(",")
     listkoordinat.append((float(templist[0]), float(templist[1])))
+nexti = i+1
 for i in range(nexti, len(read)):
     templist = read[i].split(" ")
     for j in range(len(templist)):
         templist[j] = int(templist[j])
     matrixjalan.append(templist)
-n = len(listkoordinat)
-#matrix buat jarak eclidean
+
+#matrix buat jarak euclidean (jarak garis lurus antar simpul)
+#Sebenarnya karena bumi ga datar, pakai haversine formula buat jarak antar 2 simpul, ga euclidean
 matrixgeo = [[0 for j in range(n)] for i in range(n)]
 for i in range(n):
     for j in range(n):
-        matrixgeo[i][j] = euclideanDistance(listkoordinat[i], listkoordinat[j])
+        matrixgeo[i][j] = haversineDistance(listkoordinat[i], listkoordinat[j])
 
-#list[prediksi, sejauhini, path, nama]
-'''
-#coba2 gambar graf
-#Gambar graf
-graf = GraphVisualization()
-for i in range(n):
-    for j in range(n):
-        if (matrixjalan[i][j]):
-            graf.addEdge(nama[i], nama[j], matrixgeo[i][j])
-graf.visualize()
-'''
 listlat = []
 listlon = []
 for i in range(n):
     listlat.append(listkoordinat[i][0])
     listlon.append(listkoordinat[i][1])
+#Buat map
 map = MapV(listlat, listlon, nama)
 
-
+#Isi jalur di map
 for i in range(n):
     for j in range(n):
         listlat = []
@@ -111,38 +78,47 @@ while(1):
         print()
     else:
         break
+
+#Tambah simpul awal akhir di map
 nwal = [entry]
 latawal = [listkoordinat[nama.index(entry)][0]]
 lonawal = [listkoordinat[nama.index(entry)][1]]
 nakhir = [target]
 latakhir = [listkoordinat[nama.index(target)][0]]
 lonakhir = [listkoordinat[nama.index(target)][1]]
-
 map.tambahAwal(latawal, lonawal, nwal)
 map.tambahAkhir(latakhir, lonakhir, nakhir)
-#Mulai algo
-kawal = nama.index(entry)
-ktujuan = nama.index(target)
+
+#Mulai algo A*
+kawal = nama.index(entry) #koordinat awal
+ktujuan = nama.index(target) #koordinat tujuan
+#list awal
+#list[estimasi, sejauhini, path, nama]
 awal = [matrixgeo[kawal][ktujuan] ,0, [entry], entry]
+
+#prioqueue untuk A*, prioritas dari estimasi
 listjalan = PrioQueue()
-listjalan.enqueue(awal)
+listjalan.enqueue(awal) #prioqueue dari list
 while not listjalan.isEmpty():
-    popped = listjalan.dequeue()
-    if (popped[3] == target):
+    popped = listjalan.dequeue() #dequeue dari list
+    if (popped[3] == target): #kalau sudah ketemu target di end
         break
-    path = popped[2]
-    indeks = nama.index(popped[3])
+    path = popped[2] #ambil path sejauh ini
+    indeks = nama.index(popped[3]) #ambil indeks nama dari yang didequeue
     for i in range(len(matrixjalan[indeks])):
-        if (matrixjalan[indeks][i]):
+        if (matrixjalan[indeks][i]): #jika ada jalur
             sofar = popped[1] + matrixgeo[indeks][i]
             isipath = []
             for j in range(len(path)):
                 isipath.append(path[j])
             isipath.append(nama[i])
             grup = [matrixgeo[i][ktujuan] + sofar, sofar, isipath, nama[i]]
-            listjalan.enqueue(grup)
-hasil = popped[2]
-distance = popped[1]
+            listjalan.enqueue(grup) #Masukkan ke prioqueue
+
+hasil = popped[2] #Path dari awal ke akhir
+distance = popped[1] #total jarak akhir
+
+#memasukkan hasil ke map
 listlathasil = []
 listlonhasil = []
 namahasil = []
@@ -154,28 +130,13 @@ for i in range(len(hasil)):
 
 map.tambahjalurhasil(listlathasil,listlonhasil, hasil)
 
-#output dari node ke node
-print("Jalur (node):")
+#output dari jalur dan distancenya
+print("Jalur terpendek:")
 for i in hasil:
     if i == target:
         print(i)
     else:
         print(i, end = "-")
-print("Panjang jalur:" , '{0:.3g}'.format(distance))
+print("Panjang jalur:", distance, "km")
 
-map.visualize()
-
-'''#output node-sisi-node-dst
-print()
-print("Jalur (jalan + node):")
-for i in range(len(hasil) - 1):
-    print(hasil[i], end= "-")
-    a = hasil[i]
-    b = hasil[i+1]
-    ka = nama.index(a)
-    kb = nama.index(b)
-    print(matrixnama[ka][kb], end = "-")
-print(hasil[i+1])
-print("Panjang jalur:" , distance)'''
-
-#graf.keep()
+map.visualize() #panggil map
